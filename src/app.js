8f21c20cbb3704b6c67f49e4e36e8306cbdd58e3
Jewel <jewel.etc@gmail.com>
2020-11-24 08:23:29 +0530

    App title modified

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.use(express.static(publicDirectoryPath));
 
 app.get('', (req, res) => {
     res.render('Index.hbs', {
-        title: 'Weather Application',
+        title: 'Weather and Location App',
         name: 'Jewel'
     })
 })
